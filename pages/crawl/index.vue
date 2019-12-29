e32466a9ff964d5df2ddab0f8ce636beb279fa9d
lixiaolong763 <lixiaolong763@pingan.com.cn>
2019-12-29 11:05:28 +0800

    迁移文章提取功能

---
+++ pages/crawl/index.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="crawl-article-page">
+    <div class="page-header">
+      <p class="desc">请输入要提取的文章链接</p>
+    </div>
+    <div class="page-body">
+      <el-form ref="form" :model="ruleForm" :rules="rules">
+        <el-row type="flex" class="row" justify="space-between">
+          <el-col :xs="24" :sm="24" :md="4" :lg="4" :xl="4">
+            <el-form-item prop="template">
+              <el-select
+                v-model="ruleForm.template"
+                placeholder="请选择"
+                class="template-selector"
+              >
+                <el-option
+                  v-for="item in templateOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                ></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="19" :lg="19" :xl="19">
+            <el-form-item prop="url">
+              <el-input
+                v-model="ruleForm.url"
+                placeholder="请输入文章链接"
+              ></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+
+      <el-button ref="loadingTarget" @click="formValidate" type="primary"
+        >提取文章</el-button
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  header: {
+    title: `文章提取`,
+    titleTemplate: '%s - DJL箫氏的博客!',
+    htmlAttrs: {
+      lang: 'en',
+      amp: true
+    }
+  },
+  components: {},
+  data() {
+    return {
+      templateOptions: [
+        { label: '掘金', value: 1 },
+        { label: 'Github', value: 2 },
+        { label: '简书', value: 3 }
+      ],
+      ruleForm: {
+        url: '',
+        template: 1
+      },
+      rules: {
+        url: [
+          { required: true, message: '请输入待提取文章链接', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  mounted() {},
+  methods: {
+    formValidate() {
+      if (this.$refs.form) {
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            this.crwalArticle();
+          } else {
+            return false;
+          }
+        });
+      }
+    },
+    crwalArticle() {
+      const { xhrInstance } = this.$http({
+        url: `/crawl`,
+        method: 'get',
+        data: {
+          template: this.ruleForm.template,
+          articleUrl: this.ruleForm.url
+        },
+        loading: this.$refs.loadingTarget.$el,
+        showSuccessMsg: false,
+        showErrorMsg: true
+      });
+
+      return xhrInstance.then(
+        (data) => {
+          this.$router.push(`/post/${data._id}`);
+        },
+        () => {}
+      );
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.crawl-article-page {
+  .page-header {
+    .desc {
+      font-size: 14px;
+      color: #999;
+    }
+  }
+  .tempalte-selector {
+    width: 100%;
+  }
+  .page-body {
+    .row {
+      flex-wrap: wrap;
+    }
+  }
+}
+</style>
