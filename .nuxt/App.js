487970808a868f15a515d7b06c6edba35f2101f6
djlxs <1281233206@qq.com>
2020-10-06 18:56:03 +0800

    build

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -165,10 +165,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
