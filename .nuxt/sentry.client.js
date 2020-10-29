4f6749abc5ab9d127f8dda2274d29f3ddb226d95
djlxs <1281233206@qq.com>
2020-10-29 22:42:45 +0800

    feat: 发布

--- .nuxt/sentry.client.js
+++ .nuxt/sentry.client.js
@@ -5,7 +5,7 @@ import { Dedupe, ExtraErrorData, ReportingObserver, RewriteFrames, Vue } from '@
 
 export default function (ctx, inject) {
   /* eslint-disable object-curly-spacing, quote-props, quotes, key-spacing, comma-spacing */
-  const config = {"dsn":"https:\u002F\u002F2b7e433fe9e940c8ab342f1e0a0ed063@o139930.ingest.sentry.io\u002F5301786","environment":"production","release":"c0559d3698c4c647ccb002931b6748621eeecedd"}
+  const config = {"dsn":"https:\u002F\u002F2b7e433fe9e940c8ab342f1e0a0ed063@o139930.ingest.sentry.io\u002F5301786","environment":"production","release":"26c50bfbdf6c6f56c80d95ea01eb6297b86e59e0"}
   config.integrations = [
     new Dedupe({}),
     new ExtraErrorData({}),
